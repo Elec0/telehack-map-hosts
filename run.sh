adf6577aa2cba510d25765391c447525da660d63
Elec0 <elec08215@gmail.com>
2019-09-17 14:13:54 -0700

    Removing stuff that shouldn't be public

--- run.sh
+++ run.sh
@@ -3,25 +3,6 @@
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
-if [ ! -z "$1" ]
-then
-  if [ "$1" == "get-hosts" ]
-  then
-    echo "Get hosts"
-    python python/get_hosts.py
-    echo "Get Connections"
-    python python/get_connections.py
-    echo "Parse for D3"
-    python python/parse_for_d3.py
-    echo "Copy connections file"
-    rm connections-formatted.json
-    cp python/connections-formatted.json ./
-  else
-    echo "Use get-hosts to retrieve hosts list from your connection."
-    exit
-  fi
-fi
-
 python -m SimpleHTTPServer 8080 &
 
 open http://localhost:8080
